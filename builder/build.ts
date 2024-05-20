59ced79648a1a893bed6adde53c7780b2bda6ed0
feederbox826 <feederbox826@users.noreply.github.com>
2024-05-20 19:25:27 -0400

    partial build

--- builder/build.ts
+++ builder/build.ts
@@ -32,9 +32,9 @@ interface LocalSidecar {
     id: string // internal id of plugin
     path?: string // override path to plugin if not id
     description?: string // override description if not in index
-    readme: string // path to readme file
-    author: string // author of plugin
-    screenshots: string[] // screenshots of plugin
+    readme?: string // path to readme file
+    author?: string // author of plugin
+    screenshots?: string[] // screenshots of plugin
 }
 
 // iterate over folder
@@ -97,10 +97,10 @@ class Plugin {
     description: string // description of plugin if available
     index: string // index url of collection
     repo: string // repository of collection
-    author: string // author of collection
+    author: string // author of plugin
     path: string // path to plugin in repository
     screenshots: string[] // screenshots of plugin
-    readme: string | undefined // readme file
+    readme: string | boolean | undefined // readme file
     base_path: string // path to plugins/ folder
     repo_path: string // path to repository in the format of owner/repo
 
@@ -112,13 +112,13 @@ class Plugin {
             ?? "No Description Provided"
         this.index = defaults.index
         this.repo = defaults.global_repo
-        this.author = defaults?.global_author
-            ?? sidecar?.author // overriden author
-            ?? "Not Attributed" // no author
+        this.author = sidecar?.author
+            ?? defaults?.global_author // fall back to global author
+            ?? "No Author" // if no author
         this.path = sidecar?.path
             ?? index.id // default to ID
         this.screenshots = sidecar?.screenshots ?? []
-        this.readme = sidecar?.readme // readme file
+        this.readme = sidecar?.readme ?? true // readme file
         this.base_path = defaults.base_path ?? "main/plugins"
         this.repo_path = `${this.base_path}/${this.path}`
     }
@@ -127,15 +127,19 @@ class Plugin {
         // pre prepared values
         const folderPath = `https://github.com/${this.repo}/tree/${this.repo_path}`
         const filePath = `https://github.com/${this.repo}/blob/${this.repo_path}`
-        const readme = this.readme
-            ? `View [README.md](${filePath}/README.md){target=_blank}`
-            : "No README"
+        // if false, no readme
+        // if true, default readme
+        // otherwise, follow path or url
+        const readme = typeof(this.readme) == "string" // if readme is string
+            ? this.readme.includes("http") // if link, add target blank
+                ? `[README](${this.readme}){target=_blank}`
+                : `[README](${filePath}/${this.readme}){target=_blank}`
+            : this.readme // readme is boolean
+                ? `[README](${filePath}/README.md){target=_blank}` // default path
+                : "No README"
         const screenshots = this.screenshots
             .map(screenshot => `![${this.name} screenshot](${screenshot})  `)
             .join("\n")
-        const author = this.author.includes("http") // if includes link, add target blank
-            ? `${this.author}{target=_blank}`
-            : this.author
         // ugly formatted markdown
         return `
 ### [${this.name}](${folderPath}){target=_blank}
@@ -156,7 +160,7 @@ class Plugin {
 
 === "Author"
 
-    ${author}
+    ${this.author.replace(/(\[.+\](http.+)\s|,)/g, '$1{target=_blank}')}
 
 === "Screenshots"
     ${screenshots}`
